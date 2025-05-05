2cc9ad03c79227e5d5e874c03f8b1538d6dcf9fd
tluxy <tluxy@MSI>
2025-05-05 18:48:07 +1000

    Add project files.

---
+++ ROIStaffDirectoryApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ROIStaffDirectoryApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ROIStaffDirectoryApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ROIStaffDirectoryApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
